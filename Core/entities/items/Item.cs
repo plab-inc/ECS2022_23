b976cc2e3cb0480145661795c8fed6ed515f1ad9
Laura <laura.seifert002@stud.fh-dortmund.de>
2022-11-10 13:40:33 +0100

    created abstract entity, character and item class

---
+++ Core/entities/items/Item.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGameLevelGenerator.Core;
+
+namespace ECS2022_23.Core.entities.items;
+
+public abstract class Item : Entity
+{
+
+    protected Item(Texture2D texture) : base(texture)
+    {
+    }
+}
