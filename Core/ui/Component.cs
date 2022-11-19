a4a924e3419e0cbd3eaa237ab8049db0003d7bea
Laura <seifert.laura@gmx.de>
2022-11-19 13:18:08 +0100

    components can be added and drawn for the ui

---
+++ Core/ui/Component.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ECS2022_23.Core.ui;
+
+public abstract class Component
+{
+    public Rectangle SourceRec;
+    public Rectangle DestinationRec;
+    public Labels Label;
+
+    protected Component(Rectangle sourceRec)
+    {
+        SourceRec = sourceRec;
+    }
+
+    public abstract void Draw(SpriteBatch spriteBatch);
+}
+
+public enum Labels
+{
+    HpIcon,
+    CoinIcon,
+    XpIcon,
+    Heart,
+    TopContainer
+}
