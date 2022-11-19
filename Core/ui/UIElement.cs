0177907d4b8849b31b7ff4d7e3e85015b2aea73f
Laura <seifert.laura@gmx.de>
2022-11-19 13:20:11 +0100

    elements for textures

---
+++ Core/ui/UIElement.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ECS2022_23.Core.ui;
+
+public class UiElement : Component
+{
+    private Texture2D _texture;
+ 
+    public UiElement(Rectangle sourceRec, Texture2D texture, Labels label) : base(sourceRec)
+    {
+        _texture = texture;
+        this.Label = label;
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, DestinationRec, SourceRec, Color.White);
+    }
+    
+}
+
