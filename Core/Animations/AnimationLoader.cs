b40c5f08de6073c8a716dde2ae008e005958a9f9
Laura <laura.seifert002@stud.fh-dortmund.de>
2022-11-27 14:25:46 +0100

    reusing content and texture now

--- Core/Animations/AnimationLoader.cs
+++ Core/Animations/AnimationLoader.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -7,60 +9,96 @@ namespace ECS2022_23.Core.Animations;
 
 public static class AnimationLoader
 {
-    public static Dictionary<string, Animation> LoadPlayerAnimations(ContentManager content)
+    private static ContentManager _content;
+    private static Texture2D _texture;
+    public static Dictionary<string, Animation> PlayerAnimations { get; private set; }
+    public static Dictionary<string, Animation> SwordAnimations { get; private set; }
+    public static Animation KnifeAnimation { get; private set; }
+    public static Animation StickAnimation { get; private set; }
+    public static Animation PhaserAnimation { get; private set; }
+    public static Animation CrowbarAnimation { get; private set; }
+    public static void Load(ContentManager content)
     {
-        return new Dictionary<string, Animation>()
+        if (!Directory.Exists("Content")) throw new DirectoryNotFoundException();
+        _content = content;
+        _texture = _content.Load<Texture2D>("sprites/spritesheet");
+        LoadPlayerAnimations();
+        LoadWeaponAnimations();
+        LoadSwordAnimations();
+    }
+    
+    private static void LoadPlayerAnimations()
+    {
+        PlayerAnimations = new Dictionary<string, Animation>()
         {
             {
                 "WalkUp",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 6, new Vector2(1, 5), true)
+                new Animation(_texture, 16, 16, 6, new Vector2(1, 5), true)
             },
             {
                 "WalkRight",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 6, new Vector2(1, 4), true)
+                new Animation(_texture, 16, 16, 6, new Vector2(1, 4), true)
             },
             {
                 "WalkLeft",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 6, new Vector2(1, 4), true, true,
+                new Animation(_texture, 16, 16, 6, new Vector2(1, 4), true, true,
                     false)
             },
             {
                 "WalkDown",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 6, new Vector2(1, 3), true)
+                new Animation(_texture, 16, 16, 6, new Vector2(1, 3), true)
             },
             {
                 "AttackRight",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(6, 6), false)
+                new Animation(_texture, 16, 16, 3, new Vector2(6, 6), false)
             },
             {
                 "AttackLeft",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(6, 6), false, true, false)
+                new Animation(_texture, 16, 16, 3, new Vector2(6, 6), false, true, false)
+            },
+            {
+                "AttackUp",
+                new Animation(_texture, 16, 16, 2, new Vector2(9, 6), false)
+            },
+            {
+                "AttackDown",
+                new Animation(_texture, 16, 16, 2, new Vector2(11, 6), false)
             },
             {
                 "Default",
-                new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 7, new Vector2(1, 2), true)
+                new Animation(_texture, 16, 16, 7, new Vector2(1, 2), true)
             }
         };
     }
 
-    public static Animation LoadBasicWeaponAnimation(ContentManager content)
+    private static void LoadWeaponAnimations()
     {
-        return new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(13, 6), false);
+        CrowbarAnimation = new Animation(_texture, 16, 16, 3, new Vector2(16, 6), false);
+        KnifeAnimation = new Animation(_texture, 16, 16, 3, new Vector2(13, 5), false);
+        PhaserAnimation = new Animation(_texture, 16, 16, 3, new Vector2(16, 5), false);
+        StickAnimation = new Animation(_texture, 16, 16, 3, new Vector2(19, 6), false);
     }
-    public static Animation LoadCrowbarAnimation(ContentManager content)
+
+    private static void LoadSwordAnimations()
     {
-        return new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(16, 6), false);
-    }
-    public static Animation LoadKnifeAnimation(ContentManager content)
-    {
-        return new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(13, 5), false);
-    }
-    public static Animation LoadPhaserAnimation(ContentManager content)
-    {
-        return new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(16, 5), false);
-    }
-    public static Animation LoadStickAnimation(ContentManager content)
-    {
-        return new Animation(content.Load<Texture2D>("sprites/spritesheet"), 16, 16, 3, new Vector2(19, 6), false);
+        SwordAnimations = new Dictionary<string, Animation>()
+        {
+            {
+                "AttackRight",
+                new Animation(_texture, 16, 16, 3, new Vector2(13, 6), false)
+            },
+            {
+                "AttackLeft",
+                new Animation(_texture, 16, 16, 3, new Vector2(13, 6), false, true, false)
+            },
+            {
+                "AttackUp",
+                new Animation(_texture, 16, 16, 3, new Vector2(13, 6), false, false, false, -90)
+            },
+            {
+                "AttackDown",
+                new Animation(_texture, 16, 16, 3, new Vector2(13, 6), false, false, false, 90)
+            }
+        };
     }
 }
