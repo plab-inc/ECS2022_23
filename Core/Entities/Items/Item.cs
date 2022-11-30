7dfb6742f68c00f7d35fcff7167b8b50ddfc9bc6
Laura <laura.seifert002@stud.fh-dortmund.de>
2022-11-30 21:49:23 +0100

    draw icon method with source rec

--- Core/Entities/Items/Item.cs
+++ Core/Entities/Items/Item.cs
@@ -5,13 +5,24 @@ namespace ECS2022_23.Core.Entities.Items;
 
 public abstract class Item : Entity
 {
-
-    protected Item(Vector2 spawn,Texture2D texture) : base(spawn, texture)
+    public Rectangle SourceRect { get; }
+    protected Item(Vector2 spawn,Texture2D texture, Rectangle sourceRect) : base(spawn, texture)
     {
+        SourceRect = sourceRect;
     }
 
     public virtual void Use()
     {
         
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture, Position, SourceRect, Color.White);
+    }
+    
+    public void DrawIcon(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture, Position, SourceRect, Color.White);
     }
 }
