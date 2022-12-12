298342ecd1b764ba40a1682ee2c8b12f028d85ed
Laura <laura.seifert002@stud.fh-dortmund.de>
2022-12-12 15:47:46 +0100

    range attribute not necessary anymore

--- Core/Entities/Items/Weapon.cs
+++ Core/Entities/Items/Weapon.cs
@@ -9,17 +9,15 @@ namespace ECS2022_23.Core.Entities.Items;
 public class Weapon : Item
 {
     public float DamagePoints = 5;
-    public float Range { get; }
     public WeaponType WeaponType = WeaponType.CLOSE;
 
     public Weapon(Vector2 spawn, Texture2D texture, Dictionary<string, Animation> animations, Rectangle sourceRect) : base(spawn, texture, sourceRect)
     {
         Animations = animations;
     }
-    public Weapon(Vector2 spawn, Texture2D texture, Dictionary<string, Animation> animations, Rectangle sourceRect, WeaponType type, float range) : base(spawn, texture, sourceRect)
+    public Weapon(Vector2 spawn, Texture2D texture, Dictionary<string, Animation> animations, Rectangle sourceRect, WeaponType type) : base(spawn, texture, sourceRect)
     {
         Animations = animations;
-        Range = range;
         WeaponType = type;
     }
     
