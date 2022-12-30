951fefaa7a0317170444a0e13d5393573ca42969
Ephixis <L.Brinkmoeller@gmx.de>
2022-12-30 11:15:04 +0100

    Removed old Code from Chase.cs

--- Core/Entities/Characters/enemy/enemyBehavior/Chase.cs
+++ Core/Entities/Characters/enemy/enemyBehavior/Chase.cs
@@ -20,46 +20,4 @@ public class Chase : Behavior
         
         return Vector2.Zero;
     }
-    
-    /*
-    public override Vector2 Move(Vector2 position, float velocity)
-    {
-        
-        Vector2 change = Vector2.Zero;
-        
-        if (position.X < Target.Position.X)
-        {
-            change.X += velocity;
-            if (position.X + change.X > Target.Position.X)
-                change.X = (position.X - change.X) + Target.Position.X;
-        }
-        else if (position.X > Target.Position.X)
-        {
-            change.X -= velocity;
-            if (position.X + change.X < Target.Position.X)
-                change.X = (-position.X - change.X) + Target.Position.X;
-        }
-        if (position.Y < Target.Position.Y)
-        {
-            change.Y += velocity;
-            if (position.Y + change.Y > Target.Position.Y)
-                change.Y = (position.Y - change.Y) + Target.Position.Y;
-        }
-        else if (position.Y > Target.Position.Y)
-        {
-            change.Y -= velocity;
-            if (position.Y + change.Y < Target.Position.Y)
-                change.Y = (-position.Y - change.Y) + Target.Position.Y;
-        }
-        
-        Debug.WriteLine(change);
-        
-        if (!Owner.Collides(change))
-        {
-            return change;
-        }
-        return change;
-    }
-    */
-    
 }
